e5660d7c8062c864a538ffa15d802bf899f63f7d
Mister Tandon <enggparveshtandon@gmail.com>
2017-02-14 12:57:28 +0530

    Developed code to demonstrate ng-show and ng-hide directive uses.

--- app/app.js
+++ app/app.js
@@ -5,7 +5,8 @@
 		"name": "Samsung Galaxy J7",
 		"price": 22,
 		"description": "The Samsung Galaxy J7 is powered by 1.5GHz octa-core Samsung Exynos 7580 processor and it comes with 1.5GB of RAM. The phone packs 16GB of internal storage that can be expanded up to 128GB via a microSD card.",
-		"canPurchase": true
+		"canPurchase": true,
+		"soldOut":false
 	};
 
 	app.controller("StoreController", function () {
