bb40d89a94fbb49f5ad8db3af66a9c7f156a5ce9
Mister Tandon <enggparveshtandon@gmail.com>
2017-02-14 23:54:24 +0530

    Developed code to implement ng-src angular directive

--- app/app.js
+++ app/app.js
@@ -6,17 +6,20 @@
 			"name": "SAMSUNG Galaxy On5 (Gold, 8 GB)",
 			"price": "6,990",
 			"description": "With a leathery feel, the Samsung Galaxy On5 has a classy look with a thin frame that provides you a comfortable steady grip.",
-			"soldOut": false
+			"soldOut": false,
+			"imgSrc": "http://jquery-plugins.net/image/plugin/thumbnail-grid-with-expanding-preview.jpg"
 		}, {
 			"name": "SAMSUNG Galaxy On Nxt (Gold, 32 GB)",
 			"price": "15,900",
 			"description": "Flaunt your style with the Samsung Galaxy On Nxt. Featuring a drool-worthy body and impressive features, this smartphone is built to perform",
-			"soldOut": true
+			"soldOut": true,
+			"imgSrc": "http://visuallightbox.com/images/demo/thumbnail_metro.jpg"
 		}, {
 			"name": "SAMSUNG Gear Fit 2 Black Smartband ",
 			"price": "9,990",
 			"description": "Looking good while you are chasing your fitness goals has never been this stylish before. With a sturdy strap, water-resistant body, seamless design and powerful processor the Samsung Gear Fit2 is as efficient as it is attractive.",
-			"soldOut": false
+			"soldOut": false,
+			"imgSrc": "https://f.vimeocdn.com/si/blog/thumb.png"
 		}];
 
 	app.controller("StoreController", function () {
