7c33ce3304a652f236dca318269b09b351d26865
Mister Tandon <enggparveshtandon@gmail.com>
2017-02-14 13:50:27 +0530

    Developed code to implement angular in-build directive uses like ng-show, ng-hide, ng-repeat etc.

--- app/app.js
+++ app/app.js
@@ -1,16 +1,26 @@
 (function () {
 
 	var app = angular.module("store", []);
-	var gem = {
-		"name": "Samsung Galaxy J7",
-		"price": 22,
-		"description": "The Samsung Galaxy J7 is powered by 1.5GHz octa-core Samsung Exynos 7580 processor and it comes with 1.5GB of RAM. The phone packs 16GB of internal storage that can be expanded up to 128GB via a microSD card.",
-		"canPurchase": true,
-		"soldOut":false
-	};
+	var gem = [
+		{
+			"name": "SAMSUNG Galaxy On5 (Gold, 8 GB)",
+			"price": "6,990",
+			"description": "With a leathery feel, the Samsung Galaxy On5 has a classy look with a thin frame that provides you a comfortable steady grip.",
+			"soldOut": false
+		}, {
+			"name": "SAMSUNG Galaxy On Nxt (Gold, 32 GB)",
+			"price": "15,900",
+			"description": "Flaunt your style with the Samsung Galaxy On Nxt. Featuring a drool-worthy body and impressive features, this smartphone is built to perform",
+			"soldOut": true
+		}, {
+			"name": "SAMSUNG Gear Fit 2 Black Smartband ",
+			"price": "9,990",
+			"description": "Looking good while you are chasing your fitness goals has never been this stylish before. With a sturdy strap, water-resistant body, seamless design and powerful processor the Samsung Gear Fit2 is as efficient as it is attractive.",
+			"soldOut": false
+		}];
 
 	app.controller("StoreController", function () {
 
-		this.product = gem;
+		this.products = gem;
 	})
 })();
