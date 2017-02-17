6591ebcd97f0680f345316e16e3f5bb395f40c6b
Mister Tandon <enggparveshtandon@gmail.com>
2017-02-17 19:17:44 +0530

    Developed code to implement router functionality using calculator module.

--- app/routes/router.calc.module.js
+++ app/routes/router.calc.module.js
@@ -9,7 +9,14 @@ angular
 	])
 	.controller('CalculatorController', [
 		'$scope',
+		'$state',
 		CalculatorControllerFn
+	])
+	.controller('CalculatorSumController', [
+		'$scope',
+		'$stateParams',
+		'$state',
+		CalculatorSumControllerFn
 	]);
 
 /**
@@ -19,17 +26,106 @@ angular
 function RouterCalcModuleConfigFn($stateProvider) {
 
 	$stateProvider
-		.state('routeCalculator', {
+		.state(
+		'routeCalculator',
+		{
 			url: '/calc',
 			templateUrl: 'routes/router.calc.view.html',
 			controller: 'CalculatorController'
+		})
+		.state(
+		'routeCalculatorSum',
+		{
+			url: '/calc-sum/{augend}/{addend}',
+			templateUrl: 'routes/router.calc.sum.view.html',
+			controller: 'CalculatorSumController',
+			params: {
+				augend: {
+					value: '0'
+				},
+				addend: {
+					value: '0'
+				}
+			},
+			data: {
+				'multiplier': 6
+			}
 		});
 }
 
 /**
  * `CalculatorControllerFn` controls `storeFrontApp.router.calc.module` module behaviour.
  */
-function CalculatorControllerFn($scope) {
+function CalculatorControllerFn($scope, $state) {
 
 	$scope.title = 'Calculator';
+
+	/**
+	 * `$scope.calc` object contains augend and addend value.
+	 */
+	$scope.calc = {
+		'augend': 0,
+		'addend': 0
+	};
+
+	/**
+	 * `$scope.sum` function navigate application to `routeCalculatorSum` state to calculate sum.
+	 */
+	$scope.sum = function () {
+
+		$state
+			.go('routeCalculatorSum', {
+				'augend': $scope.calc.augend,
+				'addend': $scope.calc.addend
+			});
+	}
+}
+
+/**
+ * `CalculatorControllerFn` controls state `sum` behaviour.
+ */
+function CalculatorSumControllerFn($scope, $stateParams, $state) {
+
+	/**
+	 * Display all configured `states` information.
+	 * For specific `state` : $state.get('routeCalculator');
+	 */
+	console.log($state.get());
+
+	/**
+	 * `$scope.multiplier`, `routeCalculatorSum` state `data` property.
+	 */
+	$scope.multiplier = parseInt($state.current.data.multiplier, 10);
+
+	/**
+	 * `$scope.augend` i.e augend parameter for sum operation.
+	 * 
+	 * 	Alternative way to access `augend`
+	 * 	$scope.augend = parseInt($stateParams.augend, 10);
+	 */
+	$scope.augend = parseInt($state.params.augend, 10);
+
+	/**
+	 * `$scope.addend` i.e addend parameter for sum operation.
+	 * 
+	 * 	Alternative way to access `augend`
+	 * 	$scope.addend = parseInt($stateParams.addend, 10);
+	 */
+	$scope.addend = parseInt($state.params.addend, 10);
+
+	/**
+	 * `$scope.sum`, contain sum of augend and addend, then multiply by 
+	 * `routeCalculatorSum` state `data` property i.e. multiply with value 6.
+	 */
+	$scope.sum = ($scope.augend + $scope.addend) * $scope.multiplier;
+
+	/**
+	 * `$scope.goRouteCalculator` used to navigate application to
+	 * `routeCalculator` state.
+	 */
+	$scope.goRouteCalculator = function () {
+
+		$state.go('routeCalculator');
+	}
+
 }
