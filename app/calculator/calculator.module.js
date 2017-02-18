75a53ee0060c12c3a1c3f552616d60a905e0e5be
Mister Tandon <enggparveshtandon@gmail.com>
2017-02-18 16:22:53 +0530

    Developed custom provider to perform calculator multiply operation.

--- app/calculator/calculator.module.js
+++ app/calculator/calculator.module.js
@@ -23,6 +23,13 @@ angular
 					templateUrl: 'calculator/calculator.subtract.view.html',
 					controller: 'CalculatorSubtractController'
 				}
+				)
+				.state(
+				'CalculatorMultiplicationOperation', {
+					url: '/calculator-multiplication-operation',
+					templateUrl: 'calculator/calculator.multiplication.view.html',
+					controller: 'CalculatorMultiplicationController'
+				}
 				);
 		}
 	])
@@ -58,4 +65,85 @@ angular
 			}
 		}
 	])
-	;
+	.controller(
+	'CalculatorMultiplicationController',
+	[
+		'$scope',
+		'CalculatorService',
+		function ($scope, CalculatorService) {
+
+			$scope.title = 'Calculator multiplication operation';
+
+			$scope.product = null;
+
+			/**
+			 * `$scope.multiplicationObj` object is reference for `ng-model`:multiplicand|multiplier.
+			 */
+			$scope.multiplicationObj = {
+				'multiplicand': 0,
+				'multiplier': 0
+			};
+
+			/**
+			 * `$scope.multiplyNumbers` function is used to get product of two numbers.
+			 */
+			$scope.multiplyNumbers = function () {
+
+				CalculatorService._multiplication(
+					$scope.multiplicationObj.multiplicand,
+					$scope.multiplicationObj.multiplier,
+					function (result) {
+						$scope.product = result;
+					});
+
+			}
+		}
+	])
+	.config([
+		'CalculatorServiceProvider', function (CalculatorServiceProvider) {
+
+		}
+	])
+	.provider(
+	'CalculatorService',
+	function () {
+
+		this.$get = [
+			'$log',
+			'$http',
+			function ($log, $http) {
+
+				var oCalculatorService = {};
+
+				oCalculatorService._multiplication = function (multiplicand, multiplier, callBack) {
+
+					$http({
+						url: "http://localhost:3002/calculator/multiply/" + multiplicand + "/" + multiplier,
+						method: 'GET'
+					}).then(
+
+						function (resultData) {
+
+							callBack(resultData.data.product);
+						},
+						function (errResult) {
+
+							console.log('Error Occured: Something went wrong, try after some time.');
+						}
+						);
+
+					return parseInt(multiplicand, 10) * parseInt(multiplier, 10);
+				}
+
+				return oCalculatorService;
+			}
+		];
+	}
+	);
+
+
+
+
+
+
+
