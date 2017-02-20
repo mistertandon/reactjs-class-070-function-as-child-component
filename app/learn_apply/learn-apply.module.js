f2d84c0e1ca793666fcc1770b1d8b06f54236ff9
Mister Tandon <enggparveshtandon@gmail.com>
2017-02-20 19:53:08 +0530

    Developed code to implement angularjs  method.

--- app/learn_apply/learn-apply.module.js
+++ app/learn_apply/learn-apply.module.js
@@ -28,6 +28,52 @@ angular
 		function ($scope) {
 
 			$scope.title = 'AngularJS apply demo';
+
+			$scope.multiplicand = 0;
+			$scope.multiplier = 0;
+			$scope.productr = 0;
+			$scope.productrAlias = 0;
+
+			/**
+			 * `$scope.calcMultiplication` performs multiplication opeartion.
+			 */
+			$scope.calcMultiplication = function () {
+
+				$scope.productr = $scope.multiplicand * $scope.multiplier;
+			}
+
 		}
 	]
 	);
+
+/**
+ * `calcMultiplicationApplyFn` perform multiplication from javascript context, After it use AngularJS $apply
+ * to kicked digest process.
+ */
+var calcMultiplicationApplyFn = function () {
+
+	var $scope = angular.element($('#angularjs-apply-demo')).scope();
+
+	/**
+	 * 
+		$scope.productrAlias = $scope.multiplicand * $scope.multiplier;
+		$scope.$apply();
+
+	 * 
+	 */
+
+	$scope.$apply(function () {
+
+		$scope.productrAlias = $scope.multiplicand * $scope.multiplier;
+	});
+
+}
+
+
+
+
+
+
+
+
+
