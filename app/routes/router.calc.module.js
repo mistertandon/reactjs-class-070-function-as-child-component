d59e660501688a16760803cd45459d9f31356a04
Mister Tandon <enggparveshtandon@gmail.com>
2017-02-21 12:36:24 +0530

    Developed code to implement state resolve functionality.

--- app/routes/router.calc.module.js
+++ app/routes/router.calc.module.js
@@ -25,6 +25,20 @@ angular
 					url: '/calc-sum/{augend}/{addend}',
 					templateUrl: 'routes/router.calc.sum.view.html',
 					controller: 'CalculatorSumController',
+					resolve: {
+						sumResolveData: [
+							'CalculatorFactory',
+							'$stateParams',
+							function (CalculatorFactory, $stateParams) {
+
+								var augend, addend;
+
+								augend = $stateParams.augend;
+								addend = $stateParams.addend;
+
+								return CalculatorFactory._sum(augend, addend);
+							}]
+					},
 					params: {
 						augend: {
 							value: '0'
@@ -36,6 +50,34 @@ angular
 					data: {
 						'multiplier': 6
 					}
+				});
+		}
+	])
+	.run(
+	[
+		'$rootScope',
+		function ($rootScope) {
+
+			$rootScope.$on(
+				'$stateChangeStart',
+				function (e, toState, toParams, fromState, fromParams, options) {
+
+					if (toState.resolve) {
+
+						$rootScope.isLoading = true;
+					}
+					console.log('$stateChangeStart');
+				});
+
+			$rootScope.$on(
+				'$stateChangeSuccess',
+				function (e, toState, toParams, fromState, fromParams) {
+
+					if (toState.resolve) {
+
+						$rootScope.isLoading = false;
+					}
+					console.log('$stateChangeSuccess');
 				});
 		}
 	]);
